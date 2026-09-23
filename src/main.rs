bf7bb6d150e5bbe6b84efc0857a18eba31a55a0b
iKi <180210171+nff747@users.noreply.github.com>
2026-09-23 20:42:25 -0300

    Add Point struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Point {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl Point {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Self { x, y, z }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("BVH Builder for Splats");
 }
